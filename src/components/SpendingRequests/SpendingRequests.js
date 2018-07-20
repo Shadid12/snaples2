79d964c26c82a60a4b4ddc25a4bd70fb73b38cc8
shadid12 <shadidhaque2012@gmail.com>
2018-07-19 20:40:51 -0400

    fully functional but not user friendly

--- src/components/SpendingRequests/SpendingRequests.js
+++ src/components/SpendingRequests/SpendingRequests.js
@@ -7,6 +7,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import queryString  from '../../lib/query-string-master';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 import Campaign from '../../etherium/campaign';
 import web3 from '../../etherium/web3';
@@ -16,7 +17,9 @@ export default class SpendingRequests extends Component {
     state = {
         requestCount: '',
         contributersCount: '',
-        requests: []
+        requests: [],
+        contribution: '',
+        loading: false
     }
 
     async componentDidMount() {
@@ -41,6 +44,58 @@ export default class SpendingRequests extends Component {
         })
     }
 
+    onApprove = async(id) => {
+        this.setState({loading: true});
+
+        const parsed = queryString.parse(window.location.search);
+        const campaign = Campaign(parsed.address);
+        const accounts = await web3.eth.getAccounts();
+        try {
+            await campaign.methods.approveRequest(id).send({
+                from: accounts[0]
+            });
+        } catch(err) {
+            console.log('onApprove Error', err);
+        }
+        console.log('Approved');
+        this.setState({loading: false});
+    }
+
+    onFinalize = async (id) => {
+        this.setState({loading: true});
+        const parsed = queryString.parse(window.location.search);
+        const campaign = Campaign(parsed.address);
+        const accounts = await web3.eth.getAccounts();
+        try {
+            await campaign.methods.finalizeRequest(id).send({
+                from: accounts[0]
+              });
+        } catch (err) {
+            console.log('--->', err);
+        }
+        console.log('Spend Request Finalized', )
+        this.setState({loading: false});
+    };
+
+    contributeToCampaign = async() => {
+        const parsed = queryString.parse(window.location.search);
+
+        const campaign = Campaign(parsed.address);
+        this.setState({ loading: true, errorMessage: '' });
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+              from: accounts[0],
+              value: web3.utils.toWei(this.state.contribution, 'ether')
+            });
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
+
+        this.setState({loading: false})
+    }
+
     render() {
         return(
             <div>
@@ -57,9 +112,9 @@ export default class SpendingRequests extends Component {
                         </TableHead>
                         <TableBody>
                             {
-                                this.state.requests.map(n => {
+                                this.state.requests.map((n, index) => {
                                     return(
-                                        <TableRow key={n.contractAddress}>
+                                        <TableRow key={n.address}>
                                             <TableCell>
                                                 {n.description}
                                             </TableCell>
@@ -70,12 +125,20 @@ export default class SpendingRequests extends Component {
                                                 {n.recipient.substring(0, 6)} ...
                                             </TableCell>
                                             <TableCell>
-                                                <Button variant="contained" >
+                                                <Button variant="contained"
+                                                    onClick={ () => {
+                                                        this.onApprove(index)
+                                                    }}
+                                                >
                                                     Approve
                                                 </Button>
                                             </TableCell>
                                             <TableCell>
-                                                <Button variant="contained" >
+                                                <Button variant="contained"
+                                                    onClick={ () => {
+                                                        this.onFinalize(index)
+                                                    }}
+                                                >
                                                     Finalize
                                                 </Button> 
                                             </TableCell>
@@ -86,6 +149,25 @@ export default class SpendingRequests extends Component {
                         </TableBody>
                     </Table>
                 </Paper>
+
+                <div className='txt-field'>
+                        <TextField
+                            label='ETH'
+                            value={this.state.contribution}
+                            type="number"
+                            onChange={event =>
+                                this.setState({ contribution: event.target.value })}
+                        />
+                </div>
+
+                <Button
+                        variant="contained" 
+                        color="primary"
+                        onClick={this.contributeToCampaign}
+                        disabled={this.state.loading}
+                    >
+                        Contribute
+                </Button>
             </div>
         )
     }
